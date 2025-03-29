51629935b33099a6744e2199b560486d06197025
joaquinSolano <Jsolanoupc04@gmail.com>
2025-03-28 23:28:45 -0500

    v1.4

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment={
     production:true,
-    base:'https://api.render.com/deploy/srv-cvjn42ogjchc73d5qi10?key=94IWefo1h-k'
+    base:'https://todolist-backend-v32k.onrender.com/'
 }
