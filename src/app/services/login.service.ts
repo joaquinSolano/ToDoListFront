5a6c5ee2f4654a1cebbb0e33f5587d32a0ced540
joaquinSolano <Jsolanoupc04@gmail.com>
2025-03-28 23:25:14 -0500

    v1.3

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -3,13 +3,15 @@ import { Injectable } from '@angular/core';
 import { JwtRequest } from '../models/jwtRequest';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { tap } from 'rxjs';
+import { environment } from '../../environments/environment';
+const base_url=environment.base
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
   constructor(private http: HttpClient) {}
 login(request: JwtRequest) {
-  return this.http.post('http://localhost:8081/login', request).pipe(
+  return this.http.post(`${base_url}/login`, request).pipe(
     tap((data: any) => {
       sessionStorage.setItem('username', request.username);
       sessionStorage.setItem('token', data.jwttoken);
